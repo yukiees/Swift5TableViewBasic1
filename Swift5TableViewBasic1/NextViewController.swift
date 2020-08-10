2c60298278c4b6342ad7506c8ae7f7237a425192
yukiees <yukiii8989@gmail.com>
2020-08-11 00:17:30 +0900

    Todo1

---
+++ Swift5TableViewBasic1/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5TableViewBasic1
+//
+//  Created by 松島優希 on 2020/08/10.
+//  Copyright © 2020 松島優希. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swift5TableViewBasic1/ViewController.swift
+++ Swift5TableViewBasic1/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
